fafdb4e808feb27abc83c65b6ad3efe33a69a627
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-09-02 21:46:35 +0900

    🔥 remove 'oci_artifacts_container_repository'

--- terraform/artifacts.tf
+++
@@ -1,8 +0,0 @@
-resource "oci_artifacts_container_repository" "minecraft_container_repository" {
-  #Required
-  compartment_id = oci_identity_compartment.tsar_compartment.id
-  display_name   = "tsar-minecraft-container-repository-container-repository"
-
-  is_immutable = false
-  is_public    = false
-}
