4c8fb4e316bfafaf0bffa366a547b4ccedc28f2b
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-09-22 01:44:17 +0900

    🧱 add identity user 'hotatting' and 's1gnsgrfu' to 'tsar-compartment-maintainer-group'

--- terraform/Identity.tf
+++ terraform/Identity.tf
@@ -1,10 +1,3 @@
-## compartment
-resource "oci_identity_compartment" "tsar_compartment" {
-  compartment_id = data.sops_file.secrets.data["oci.config.tenancy_ocid"]
-  description    = "tsar compartment"
-  name           = "tsar-compartment"
-}
-
 ## identity user
 ### https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/identity_user
 resource "oci_identity_user" "user_leoleo" {
@@ -13,6 +6,22 @@ resource "oci_identity_user" "user_leoleo" {
   name           = "leoleo"
 
   email = data.sops_file.secrets.data["oci.identity-user.leoleo.email"]
+}
+
+resource "oci_identity_user" "user_hotatting" {
+  compartment_id = data.sops_file.secrets.data["oci.config.tenancy_ocid"]
+  description    = "identify user for tsar"
+  name           = "hotatting"
+
+  email = data.sops_file.secrets.data["oci.identity-user.hotatting.email"]
+}
+
+resource "oci_identity_user" "user_s1gnsgrfu" {
+  compartment_id = data.sops_file.secrets.data["oci.config.tenancy_ocid"]
+  description    = "identify user for tsar"
+  name           = "s1gnsgrfu"
+
+  email = data.sops_file.secrets.data["oci.identity-user.s1gnsgrfu.email"]
 }
 
 ## identity group
@@ -29,6 +38,16 @@ resource "oci_identity_group" "tsar_compartment_maintainer_group" {
 resource "oci_identity_user_group_membership" "tsar_compartment_maintainer_group_membership_leoleo" {
   group_id = oci_identity_group.tsar_compartment_maintainer_group.id
   user_id  = oci_identity_user.user_leoleo.id
+}
+
+resource "oci_identity_user_group_membership" "tsar_compartment_maintainer_group_membership_hotatting" {
+  group_id = oci_identity_group.tsar_compartment_maintainer_group.id
+  user_id  = oci_identity_user.user_hotatting.id
+}
+
+resource "oci_identity_user_group_membership" "tsar_compartment_maintainer_group_membership_s1gnsgrfu" {
+  group_id = oci_identity_group.tsar_compartment_maintainer_group.id
+  user_id  = oci_identity_user.user_s1gnsgrfu.id
 }
 
 ## identity policy
