67d597360dc7da4389ae710a0ce41f508bdccbf5
taiki-kuraishi <kuraishi.taiki0@gmail.com>
2025-04-27 20:38:46 +0900

    Remove minecraft container

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,13 +2,3 @@ data "sops_file" "secrets" {
   source_file = "../secret.enc.yaml"
   input_type  = "yaml"
 }
-
-variable "tsar_minecraft_container_instance_environment_variables" {
-  type = object({
-    EULA = string
-  })
-  default = {
-    EULA = "TRUE"
-  }
-  description = "Environment variables for minecraft container"
-}
